0d82ca38dda17dc1ca4fc1591cab9d44787070f8
Alex <angrylexa@mail.ru>
2022-08-22 13:54:01 +0300

    DZ27

--- Program.cs
+++ Program.cs
@@ -3,22 +3,45 @@
 // 2, 4 -> 16
 
 
+// Console.Clear();
+// Console.Write("Введите число : ");
+// int A = int.Parse(Console.ReadLine());
+// Console.Write("Введите степень : ");
+// int B = int.Parse(Console.ReadLine());
+
+// Console.WriteLine($"{A} в степень {B} = {Pow(A,B)}");
+
+// int Pow(int num, int rank)
+// {
+//     if(B==0) return 1;
+
+//     int result = num;
+//     for (int i = 2; i <= rank; i++)
+//     {
+//         result *= num;
+//     }
+//     return result;
+// }
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
 Console.Clear();
 Console.Write("Введите число : ");
-int A = int.Parse(Console.ReadLine());
-Console.Write("Введите степень : ");
-int B = int.Parse(Console.ReadLine());
+int num = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"{A} в степень {B} = {Pow(A,B)}");
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
 
-int Pow(int num, int rank)
+
+int GetSumNums(int number)
 {
-    if(B==0) return 1;
-
-    int result = num;
-    for (int i = 2; i <= rank; i++)
+    int sum = 0;
+    while (number>0)
     {
-        result *= num;
+        sum += number%10;
+        number /=10;
     }
-    return result;
+    return sum;
 }
